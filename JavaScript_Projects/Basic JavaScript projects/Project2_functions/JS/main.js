da076b30b17558249305b103161b5424f84c7d88
brianb1104 <73188664+brianb1104@users.noreply.github.com>
2020-12-17 12:19:29 -0800

    project2

---
+++ JavaScript_Projects/Basic JavaScript projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function myFunction () {
+var sentence = "I am learning"; //varible is sentence
+sentence += " a lot form this book!"; //operator to concatenate the string.
+document.getElementById("concatenate").innerHTML = sentence;
+}
