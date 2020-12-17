2f32f34bee7c495e8aafef026ccae665c58dec01
brianb1104 <73188664+brianb1104@users.noreply.github.com>
2020-12-16 18:18:43 -0800

    Basic_JavaScript projects

---
+++ JavaScript_Projects/Basic JavaScript projects/Project1_expression_alert/JS/main.js
@@ -0,0 +1,16 @@
+window.alert("HELLO,WORLD");
+document.window('HELLO,WORLD');
+var A = "This is a string";
+document.write (A);
+window.alert(A);
+System.out.print("\t I love \"Java\"programing");
+document.write("\How are you,"
++ " because I miss you, don\'t you miss me");
+var Name = "This is " + "We do it!"
+document.write(Name)
+var Love = "Donna", Babe = "Chiqui", Niece = "Destiny";
+document.write(Love);
+var red = "I'\m red hot.";
+var red = red.fontcolor("red");//using the fontcolor on a str variable
+document.write(red);
+let reg = /hi(m|s)/g;
