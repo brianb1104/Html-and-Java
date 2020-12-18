de68f046ba4c2c9ef853932cb8fab2b14a5dff80
brianb1104 <73188664+brianb1104@users.noreply.github.com>
2020-12-18 15:55:59 -0800

    step 92

---
+++ JavaScript_Projects/Basic JavaScript projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,10 @@
+function my_Dictionary() {
+    var Animal = {
+        Species: "Dog",
+        Color:"Brown",
+        Breed: "German Sheapard",
+        Age: "2",
+        Sound:"Bark!"
+    };
+    document.getElementById("Dictionary").innerHTML = Animal.Sound;
+}
