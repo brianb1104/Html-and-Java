c310871b4d32d6f43e48a6c405c7b9a3349d8e49
brianb1104 <73188664+brianb1104@users.noreply.github.com>
2020-12-17 17:49:09 -0800

    math 3

---
+++ JavaScript_Projects/Basic JavaScript projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,9 @@
+function toCelsius(fahrenheit) {
+  return (5/9) * (fahrenheit-32);
+  document.getElementById("demo").innerHTML = toCelsius(77);
+}
+
+function addition_Function() {
+  var Math = 4 + 4;
+  document.getElementById("Math").innerHTML = "4 + 4 = " + addition;
+} 
