0499d61101731c7b327a08db85d5a7cb5faa20e4
brianb1104 <73188664+brianb1104@users.noreply.github.com>
2020-12-21 12:41:52 -0800

    submit assignments 4-6

--- JavaScript_Projects/Basic JavaScript projects/Project5_comparisons_type_coercion/JS/main.js
+++ JavaScript_Projects/Basic JavaScript projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,77 +1,97 @@
-function OPerator() {
-document.write(typeof "Word");//this is a qperator the variable is Word
-}
+function Operator() {
+document.write(typeof "Word");//this is a qperator the variable is Word  
+} 
 
 function Test() {
     document.getElementById("Test").innerHTML = 0/0;//The result is NAN cause you cant divide a Zero into a zero
 }
 
-function Test() {
-    document.getElementById("my_Function").innerHTML = isNaN('This is a string');//This will display True cause its a string not a number
+function Test2() {
+    document.getElementById("Test").innerHTML = isNaN('This is a string');//This will display True cause its a string not a number
 }
 
-function Test() {
-    document.getElementById("Test_2").innerHTML = isNaN('007'); //This result is False cause 007 is a number
+function Test3() {
+    document.getElementById("Test").innerHTML = isNaN('007'); //This result is False cause 007 is a number
 }
 
-function Negative_Infinte {// displays Negative infinte
-    document.write(-3E310);
+function Not_infinte() {
+    document.write(-3E310);// displays Negative infinte
 }
 
-function Infinte {
+ function Is_infinte() {
     document.write(2E310);//displays Infinte
 }
 
+function True() {
 document.write(10 > 2);//displays True
+}
 
+function False() {
 document.write(10 < 2);//displays False
+}
 
 function division() {
     var simple_Math = 10 / 5;
-    document.getElementById("Math").innerHTML = "10 / 5 = " + simple_Math;// 10 divide by 5
-    console.log(10 / 5);//shows Divided in console
-  }
+    document.getElementById("Test").innerHTML = "10 / 5 = " + simple_Math;// 10 divide by 5
+    console.log(simple_Math);//shows Divided in console
+}
 
+  function Tring() {
   document.write("10" + 5); //displays 105
+}
 
   function Logic() {
     var simple_Math = 10 < 5;
-    document.getElementById("Math").innerHTML = "10 < 5 = " + simple_Math;
+    document.getElementById("Test").innerHTML = "10 < 5 = " + simple_Math;
     console.log(10 < 5);// display False in console
-  }
-
+}
+  function Truth() {
   document.write(10 == 10);//display True
-
+}
+  function Lie() {
   document.write(3 == 11);// displays False
-
+}
+  function Triple() {
   A = "Magnus";
   B = "Magnus";
   document.write(A === B);//displays True
-
-  C = "Magnus"
-  D = Magnus;
-  document.write(C === D);//displays False
-
+}
+function Triple2() {
+  A = "magnus"
+  B = magnus;
+  document.write(A === B);//displays False
+}
+function triple_N() {
   X = 82;
   Y = "82";
   document.write(X === Y);// displays False
+}
 
+function triple_Y() {
   S = 82;
   T = 82;
   document.write(S === T);//displays True
+}
 
+function two_A() {
   document.write(5 > 2 && 10 > 4);//displays True
+}
 
+function two_B() {
   document.write(2 > 5 && 10 > 4);//displays False
+}
 
+function straight_L() {
   document.write(10 > 15 || 10 > 4);// displays True
-
+}
+function straight_L2() {
   document.write(5 > 20 || 10 > 4);//displays False
+}
 
   function not_Function() {
-      document.getElementById("Not").innerHTML = !(20 > 10);// display False
-  }
+      document.getElementById("Test").innerHTML = !(20 > 10);// display False
+}
 
-  function not_Function() {
-      document.getElementById("Not").innerHTML = !(5 > 10);//displays True
-  }
+  function it_Function() {
+      document.getElementById("Test").innerHTML = !(5 > 10);//displays True
+}
--- JavaScript_Projects/Basic JavaScript projects/Project6_ternary__operators_constructors/JS/main.js
+++ JavaScript_Projects/Basic JavaScript projects/Project6_ternary__operators_constructors/JS/main.js
@@ -1,14 +1,14 @@
 function Ride_Function() {
     var Height, Can_ride;
     Height = document.getElementById("Height").value;
-    Can_ride (Height < 52) ? "You are too short":"You are tall enough";
+    Can_ride = (Height < 52) ? "You are too short":"You are tall enough";
     document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
 }
 
 function Age_Function() {
     var Age, Can_vote;
     Age = document.getElementById("Age").value;
-    Can_vote (Age < 18) ? "You are too young":"You are old enough";
+    Can_vote = (Age < 18) ? "You are too young":"You are old enough";
     document.getElementById("Vote").innerHTML = Can_vote + " to vote.";
 }
 
@@ -18,12 +18,44 @@ function Vehicle(Make, Model, Year, Color) {
     this.Vehicle_Year = Year;
     this.Vehicle_Color = Color;
 }
-var Jack = new Vehicle("Dodge", "Viper" 2020, "Red");
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
 var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
 var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
 function my_Function() {
     document.getElementById("Keywords_and_Constructors").innerHTML =
-    "Erik drives a " + Erik.Vehcile_Color + "-colored " + Erik.Vehicle_Model +
+    "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model +
     " manufactured in " + Erik.Vehicle_Year;
+    console.log(Erik);
 }
 
+function Television(Make, Model, Year, Class ) {
+    this.Television_Make = Make;
+    this.Television_Model = Model;
+    this.Television_Year = Year;
+    this.Television_Class = Class;
+}
+var Jack = new Television("Sony", "A8H", 2020, "OLed");
+var Emily = new Television("Samsung", "Q90T", 2019, "4K");
+var Justin = new Television("Zenith", "Silver_Finger", 1971, "Color");
+function Tube() {
+    document.getElementById("New_and_This").innerHTML =
+    "Justin watches a " + Justin.Television_Make + "color " + Justin.Television_Model +
+    " manufactured in " + Justin.Television_Year;
+}
+
+function abstract(first, last, age, eye) {//
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eye;
+  }
+
+  function count_Function() {
+      document.getElementById("Nested_Function").innerHTML = Count();
+      function Count() {
+          var Starting_point = 9;
+          function Plus_one() {Starting_point += 1;}
+          Plus_one();
+          return Starting_point;
+      }
+  }
